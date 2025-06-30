9144d74e1b9619e49eed241053e64c29d79220c9
Sreshty-Beshty <126054700+Sreshty-Beshty@users.noreply.github.com>
2025-06-30 13:52:40 -0400

    fixed cookie for theme across committee, about us page fix

--- theme_dropdown/script.js
+++ theme_dropdown/script.js
@@ -14,22 +14,25 @@ function applyTheme(theme) {
     const root = document.documentElement;
 
     if (theme === 'light') {
-        root.style.setProperty('--bg-color', '#FAF5ED');
-        root.style.setProperty('--sec-bg', '#003057');
-        root.style.setProperty('--accent-color', '#1E96A5');
-        root.style.setProperty('--sec-accent-color', '#FFCB00');
-        root.style.setProperty('--tert-bg', '#B4EBF5');
+        root.style.setProperty('--bg-color', 'rgb(250, 245, 237)');         // #FAF5ED
+        root.style.setProperty('--sec-bg', 'rgb(0, 48, 87)');               // #003057
+        root.style.setProperty('--accent-color', 'rgb(30, 150, 165)');      // #1E96A5
+        root.style.setProperty('--sec-accent-color', 'rgb(255, 203, 0)');   // #FFCB00
+        root.style.setProperty('--tert-bg', 'rgb(180, 235, 245)');          // #B4EBF5
         root.style.setProperty('--text-color', 'black');
     } else {
         root.style.setProperty('--bg-color', 'rgb(17, 17, 17)');
         root.style.setProperty('--sec-bg', 'rgb(53, 53, 53)');
-        root.style.setProperty('--accent-color', '#1E96A5');
-        root.style.setProperty('--sec-accent-color', '#FFCB00');
-        root.style.setProperty('--tert-bg', '#002F52');
-        root.style.setProperty('--text-color', '#F7F2EC');
+        root.style.setProperty('--accent-color', 'rgb(30, 150, 165)');      // #1E96A5
+        root.style.setProperty('--sec-accent-color', 'rgb(255, 203, 0)');   // #FFCB00
+        root.style.setProperty('--tert-bg', 'rgb(0, 47, 82)');              // #002F52
+        root.style.setProperty('--text-color', 'rgb(247, 242, 236)');       // #F7F2EC
     }
 }
 
+
+
+// --bg-color: rgb(250, 245, 237); --sec-bg: rgb(0, 48, 87); --accent-color: rgb(30, 150, 165); --sec-accent-color: rgb(255, 203, 0); --tert-bg: rgb(180, 235, 245); --text-color: black;
 document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = getCookie('theme') || 'dark';
     applyTheme(savedTheme);
