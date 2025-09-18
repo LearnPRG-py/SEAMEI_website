d4824c1b7ed60b4e28e01c45dea6e2eae73504a6
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-09-18 20:10:33 +0530

    Sponsors and blogs

--- media_page/script.js
+++ media_page/script.js
@@ -1,11 +1,41 @@
 // Example JSON (in reality you’d load this from a file or API)
 const updatesData = [
     {
+        title: "Humanitarian Diplomacy in a Polarised World: HRC at OakMUN 2025",
+        caption: "Navigating Challenges, Fostering Cooperation",
+        href: "../media/blog6.html",
+        image: "hrc.jpg"
+    },
+    {
+        title: "Decoding the Future: ECOSOC’s Bold Dive into Cryptocurrency Regulation",
+        caption: "The Vision: Adapt, Think Bigger, Think Forward",
+        href: "../media/blog5.html",
+        image: "fin.jpg"
+    },
+    {
+        title: "Leading OakMUN 2025 — Secretary General Soumyajit Sur Roy",
+        caption: "Giving Voice to the Unheard",
+        href: "../media/blog4.html",
+        image: "im2.jpeg"
+    },
+    {
+        title: "OAKMUN: An Insider Account of Greatness - Rishita Borah",
+        caption: "An Insider's Perspective on OAKMUN",
+        href: "../media/blog3.html",
+        image: "b1.jpg"
+    },
+    {
+        title: "Inside OAKMUN 2025 with Director General Akshat Gupta",
+        caption: "A Deep Dive into OAKMUN 2025",
+        href: "../media/blog2.html",
+        image: "b2.jpg"
+    },
+    {
         title: "What is OAKMUN?",
         caption: "An Introduction to OAKMUN",
         href: "../media/blog1.html",
-        image: "im2.jpeg"
-    }
+        image: "im4.jpeg"
+    },
 ];
 
 const feed = document.getElementById("updates-feed");
